6e7357f0811073be3d1ef0d472aebfcc745bad7c
Nexus <satanzlb@gmail.com>
2016-01-20 17:43:14 +0800

    modify the popup function

--- src/scripts/z.js
+++ src/scripts/z.js
@@ -257,11 +257,11 @@ style.innerText='.popup .btns{margin: 10px 0; text-align: center; font-size: 0; 
 '.popup.dark .wrapper .btns .btn-wrapper, .popup.ios .wrapper .btns .btn-wrapper{display: block; margin-bottom: -5px; border-top: 1px solid rgba(255, 255, 255, 0.5); padding: 0; overflow: hidden; -webkit-box-flex: 1; -webkit-box-flex: 1; -webkit-flex: 1; flex: 1; }'
 +'.popup.dark .wrapper .btns .btn-wrapper .btn, .popup.ios .wrapper .btns .btn-wrapper .btn {border: 0; margin:-1px 0;background-color: transparent; -webkit-border-radius: 0; -moz-border-radius: 0; border-radius: 0; }'
 +'.popup.dark .wrapper .btns .btn-wrapper + .btn-wrapper,  .popup.ios .wrapper .btns .btn-wrapper + .btn-wrapper {border-left: 1px solid rgba(255, 255, 255, 0.5);}'
-+'.popup.ios .wrapper {  background: rgba(255, 255, 255, 0.85); color: #333;border:1px solid #ddd; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; }'
-+'.popup.ios .wrapper .btns .btn-wrapper {border-color: #e0e0e0; }'
++'.popup.ios .wrapper {  background: rgba(255, 255, 255, 0.95); color: #333; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; }'
++'.popup.ios .wrapper .btns .btn-wrapper {border-color: #e2e2e2; }'
 +'.popup.ios .wrapper .btns .btn-wrapper .btn {color: #333; }.popup.ios .wrapper .btns .btn-wrapper .btn.ok{color: #4891DC;}'+
-'.popup.ios .wrapper .btns .btn-wrapper + .btn-wrapper {border-color: #e0e0e0; }'+
-'@-webkit-keyframes popup-show { 100% {-webkit-transform: translate(0, -60%); transform: translate(0, -60%); opacity: 1; } } @keyframes popup-show {100% {-webkit-transform: translate(0, -60%); transform: translate(0, -60%); opacity: 1; } }';
+'.popup.ios .wrapper .btns .btn-wrapper + .btn-wrapper {border-color: #e2e2e2; }'+
+'@-webkit-keyframes popup-show { 100% { opacity: 1; } } @keyframes popup-show {100% {opacity: 1; } }';
     var title = opt.title,
         content = opt.content,
         okVal = opt.okVal,
@@ -277,7 +277,7 @@ style.innerText='.popup .btns{margin: 10px 0; text-align: center; font-size: 0; 
     if (opt.theme == 'dark') {
         mask = false;
     } else if (/iOS|iPhone|Apple/i.test(opt.theme)) {
-        mask = false;
+        //mask = false;
         opt.theme = 'iOS';
     }
 
@@ -302,6 +302,7 @@ style.innerText='.popup .btns{margin: 10px 0; text-align: center; font-size: 0; 
         return typeof obj == 'function' ? true : false;
     }
 
+    document.body.appendChild(div);
     div.addEventListener('click', function(e) {
         var e = e || window.event,
             cl = e.target.classList;
@@ -313,7 +314,7 @@ style.innerText='.popup .btns{margin: 10px 0; text-align: center; font-size: 0; 
             _remove();
         }
     })
-    document.body.appendChild(div);
+
     _isFunction(beforeShow) && beforeShow();
 
     //自动关闭执行事件
